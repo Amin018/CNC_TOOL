c6977224dc95f12b0d6163e8c655661f0f5987d3
Amin018 <aminzulkarnain1806@gmail.com>
2025-10-21 14:02:48 +0800

    Add 72 Bytes limit to password

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    safe_password = password[:72]  # bcrypt limit
+    return pwd_context.hash(safe_password)
 
 def verify_password(plain_password: str, password_hash: str) -> bool:
-    return pwd_context.verify(plain_password, password_hash)
+    safe_password = plain_password[:72]
+    return pwd_context.verify(safe_password, password_hash)
