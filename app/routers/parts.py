cdb615e68e6aad5a6a0c85f7d63d233f1183af7a
Amin018 <aminzulkarnain1806@gmail.com>
2025-12-24 15:12:16 +0800

    Adjust @get api sequence

--- app/routers/parts.py
+++ app/routers/parts.py
@@ -29,6 +29,44 @@ def create_part(
 def list_parts(db: Session = Depends(database.get_db)):
     return db.query(models.Part).order_by(models.Part.id).all()
 
+
+@router.get("/search")
+def search_parts(query: str, db: Session = Depends(database.get_db)):
+    stmt = select(models.Part).where(models.Part.part_no.ilike(f"%{query}%"))
+    results = db.execute(stmt).scalars().all()
+    return [{"id": p.id, "part_no": p.part_no, "description": p.description} for p in results]
+
+
+# List part details
+@router.get("/{part_id}", response_model=schemas.PartResponse)
+def get_part(
+    part_id: int,
+    db: Session = Depends(database.get_db)
+    ):
+    part = db.query(models.Part).filter(models.Part.id == part_id).first()
+    if not part:
+        raise HTTPException(status_code=404, detail="Part not found")
+    return part
+
+
+@router.put("/{part_id}", response_model=schemas.PartResponse)
+def update_part(
+    request: schemas.PartUpdate,
+    part_id: int,
+    db: Session = Depends(database.get_db),
+    current_user: models.User = Depends(require_admin),
+):
+    part = db.query(models.Part).filter(models.Part.id == part_id).first()
+    if not part:
+        raise HTTPException(status_code=404, detail="Part not found")
+
+    part.description = request.description
+    part.package = request.package
+
+    db.commit()
+    db.refresh(part)
+    return part
+
 # Delete part (Admin only)
 @router.delete("/{part_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_part(
@@ -44,8 +82,3 @@ def delete_part(
     db.commit()
     return
 
-@router.get("/search")
-def search_parts(query: str, db: Session = Depends(database.get_db)):
-    stmt = select(models.Part).where(models.Part.part_no.ilike(f"%{query}%"))
-    results = db.execute(stmt).scalars().all()
-    return [{"id": p.id, "part_no": p.part_no, "description": p.description} for p in results]
