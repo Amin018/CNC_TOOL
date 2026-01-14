cef199e238b7895b1c7849af046a634805788227
Amin018 <aminzulkarnain1806@gmail.com>
2026-01-14 11:10:47 +0800

    Adjust acknowledge sequence

--- frontend/src/pages/ChangeoverDetail.vue
+++ frontend/src/pages/ChangeoverDetail.vue
@@ -168,7 +168,7 @@
           </button>
 
           <button
-            v-if="(user.role === 'tool' || user.role === 'admin') && changeover.concurred_by"
+            v-if="(user.role === 'tool' || user.role === 'admin') && changeover.concurred_by && !changeover.acknowledged_by"
             @click="acknowledgeChangeover"
             class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
           >
